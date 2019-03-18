c2b39c28406b88158315ea660f894d6362d865e4
Maceo Lewis <maceo.code@gmail.com>
2019-03-17 23:46:57 -0400

    Displays correct matchup format

---
+++ nba_fixture/Matchup.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nba_fixture
+{
+    public class Matchup
+    {
+        public string Date { get; set; }
+        public string HomeTeam { get; set; }
+        public string AwayTeam { get; set; }
+        public string Result { get; set; }
+        
+        public int Game_Number { get; set;}
+
+
+    }
+}
